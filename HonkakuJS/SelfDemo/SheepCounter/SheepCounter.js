e94ecf9addfeb5e8afe62b7fcb7f66c79d017e8d
Yasaka-H <yasaka245.ac@gmail.com>
2019-04-10 16:16:03 +0900

    ひつじもこもこ

---
+++ HonkakuJS/SelfDemo/SheepCounter/SheepCounter.js
@@ -0,0 +1,21 @@
+// 2.5秒ごとに羊が一匹ずつ増えていくような感じにしたい
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("SheepCount").innerHTML = "ひつじが0ひき";
+    var SC = 0; // 羊の数
+    var countUp = window.setInterval(
+    function() {
+        SC++;
+        document.getElementById("SheepCount").innerHTML = "ひつじが" + SC + "匹";
+
+        var sheep = document.createElement("img");
+        sheep.src = 'Sheep.png';
+        sheep.alt = 'Hitsuji';
+        document.getElementById("sheepArray").appendChild(sheep);
+
+        if(SC === 450) {
+            clearInterval(countUp);
+            alert("まだ寝ないの？");
+        }
+    }, 2500);
+}, false);
