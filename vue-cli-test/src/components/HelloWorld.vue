4e2552be67296f1e2241bf43d2e05b7e7bf6f87b
Yasaka-H <yasaka245.ac@gmail.com>
2019-05-09 14:14:11 +0900

    add vue cli

---
+++ vue-cli-test/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      {{ word }}
+      <el-button type="primary" v-on:click="displayHello">Primary</el-button>
+  </div>
+</template>
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+    msg: String
+    },
+    data(){
+        return {
+            word: this.msg
+        }
+    },
+    methods: {
+        displayHello() {
+            this.word = 'hello'
+        }
+    }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
